661f57ec53a5cf2b0fb298526220b3335ebd94ae
munjeni <sharecard17@gmail.com>
2013-09-20 15:57:18 +0200

    add Lionheart governor support for CPUFREQ_DEFAULT_GOVERNOR

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -372,6 +372,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
 extern struct cpufreq_governor cpufreq_gov_hotplug;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
+extern struct cpufreq_governor cpufreq_gov_lionheart;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lionheart)
 #endif
 
 
